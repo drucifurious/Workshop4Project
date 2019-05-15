c673ec0942e677f0bcf05914403a752a4157c577
Shagufta Tasneem <Shagufta.Tasneem@edu.sait.ca>
2019-05-15 12:10:34 -0600

    may 15 12:10

--- BusinessLayer/Products.cs
+++ BusinessLayer/Products.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer
 {
-    class Products
+    public class Products
     {
         public int PackageId { get; set; }
         public string ProdName { get; set; }
