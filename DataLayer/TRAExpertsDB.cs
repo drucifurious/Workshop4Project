bdf643a565df02d3e785ab5565ad5e3d97f41dbb
Ye Liu <liuye5@hotmail.com>
2019-05-28 09:19:24 -0600

    ye

--- DataLayer/TRAExpertsDB.cs
+++ DataLayer/TRAExpertsDB.cs
@@ -11,18 +11,21 @@ namespace DataLayer
     {
         public static SqlConnection GetConnection()
         {
+            //LOCAL DB
+
             //SqlConnection connection = new SqlConnection();
             //string ConnectionString = "Data Source=SoftDev;" + "Initial Catalog=TravelExperts;" + "Integrated Security=true;";
-
             //connection.ConnectionString = ConnectionString;
 
-            // Remote DB
+            //Remote DB
+
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
             builder.DataSource = "workshop4.database.windows.net";
             builder.UserID = "Ye";
             builder.Password = "Liu123456";
             builder.InitialCatalog = "TravelExperts";
-           SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            SqlConnection connection = new SqlConnection(builder.ConnectionString);
+
             connection.Open();
             return connection;
 
